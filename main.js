e3dc7fe8483f939151a174f6e99e5ca28d5e1d7d
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2023-06-07 18:25:21 +0530

    on clicking a circle, move the corresponding arrow - only move one arrow at a time

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 import "./style.css";
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-
 var circleRadius = 30;
 var circleSpacing = 10;
 var circleColors = ["red", "green", "blue", "yellow"];
+var isMoving = false;
+var fromx = 750;
+var tox = 700;
+var moveDistance = 5;
 
 // Loop through each circle
 for (var i = 0; i < 4; i++) {
@@ -12,7 +15,7 @@ for (var i = 0; i < 4; i++) {
   var x = circleRadius + circleSpacing;
   var y = circleRadius + circleSpacing + (circleRadius * 2 + circleSpacing) * i;
   draw_circle(ctx, x, y, circleRadius);
-  draw_arrow(ctx, 750, y, 700, y);
+  draw_arrow(ctx, fromx, y, tox, y);
 }
 
 function draw_circle(ctx, x, y, circleRadius) {
@@ -38,3 +41,63 @@ function draw_arrow(ctx, fromx, fromy, tox, toy) {
   ctx.fill();
   ctx.stroke();
 }
+
+function clearCanvas(x, y) {
+  ctx.clearRect(x, y - 15, 100, 30);
+}
+
+function move_arrow(i) {
+  console.log("ran");
+  var y = circleRadius + circleSpacing + (circleRadius * 2 + circleSpacing) * i;
+  if (tox < circleRadius * 2 + circleSpacing + 5) {
+    console.log(requestId);
+    cancelAnimationFrame(requestId);
+  }
+  if (isMoving && tox >= circleRadius * 2 + circleSpacing + 5) {
+    tox -= moveDistance;
+    fromx -= moveDistance;
+    clearCanvas(tox, y);
+    draw_arrow(ctx, fromx, y, tox, y);
+  }
+  var requestId = requestAnimationFrame(() => move_arrow(i));
+  if (tox < circleRadius * 2 + circleSpacing + 5) {
+    cancelAnimationFrame(requestId);
+    stopMoving();
+  }
+}
+var arrowMovedStatus = {};
+for (var i = 0; i < 4; i++) {
+  arrowMovedStatus[i] = false;
+}
+function startMoving(i) {
+  isMoving = true;
+  move_arrow(i);
+}
+
+function stopMoving() {
+  isMoving = false;
+}
+function handleClick(event) {
+  //reset starting point of x cordinates after animation ends
+  if (!isMoving) {
+    tox = 700;
+    fromx = 750;
+  }
+
+  const rect = canvas.getBoundingClientRect();
+  const clickX = event.clientX - rect.left;
+  const clickY = event.clientY - rect.top;
+  for (var i = 0; i < 4; i++) {
+    var y =
+      circleRadius + circleSpacing + (circleRadius * 2 + circleSpacing) * i;
+    const dx = x - clickX;
+    const dy = y - clickY;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    //Only moves one arrows at a time
+    if (distance <= circleRadius && !arrowMovedStatus[i] && !isMoving) {
+      arrowMovedStatus[i] = true;
+      startMoving(i);
+    }
+  }
+}
+canvas.addEventListener("click", handleClick);
