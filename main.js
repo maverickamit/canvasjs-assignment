7cc836e532b1036d0cd4651424a1c5e1b0767f9b
Amit Ghosh <29959138+maverickamit@users.noreply.github.com>
2023-06-07 15:41:40 +0530

    update draw_circle and draw_arrow functions

--- main.js
+++ main.js
@@ -11,23 +11,23 @@ for (var i = 0; i < 4; i++) {
   // Calculate the circle's position
   var x = circleRadius + circleSpacing;
   var y = circleRadius + circleSpacing + (circleRadius * 2 + circleSpacing) * i;
-
-  // Draw the circle
-  ctx.beginPath();
-  ctx.arc(x, y, circleRadius, 0, 2 * Math.PI);
-  ctx.fillStyle = circleColors[i];
-  ctx.fill();
-
-  ctx.beginPath();
-  canvas_arrow(ctx, 750, y, 700, y);
-
-  ctx.stroke();
+  draw_circle(ctx, x, y, circleRadius);
+  draw_arrow(ctx, 750, y, 700, y);
 }
 
-function canvas_arrow(ctx, fromx, fromy, tox, toy) {
+function draw_circle(ctx, x, y, circleRadius) {
+  ctx.beginPath();
+  ctx.arc(x, y, circleRadius, 0, 2 * Math.PI);
+  ctx.closePath();
+  ctx.fillStyle = circleColors[i];
+  ctx.fill();
+}
+
+function draw_arrow(ctx, fromx, fromy, tox, toy) {
   var headLen = 5; // length of head in pixels
   var headHeight = 5;
   ctx.lineWidth = 5;
+  ctx.beginPath();
   ctx.moveTo(fromx, fromy);
   ctx.lineTo(tox + headLen, toy);
   ctx.moveTo(tox, toy);
@@ -36,4 +36,5 @@ function canvas_arrow(ctx, fromx, fromy, tox, toy) {
   ctx.closePath();
   ctx.fillStyle = "black";
   ctx.fill();
+  ctx.stroke();
 }
